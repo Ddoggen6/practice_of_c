522c69a958fe6bb3c78bcafaa992f4cef20b22b3
Ddoggen6 <berninimarianosanchezperez@gmail.com>
2026-08-20 21:15:40 -0400

    A simple calculator

---
+++ Functions_calculator.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+int add(int a, int b){
+    return a + b;
+}
+
+
+
+int substract(int a, int b){
+    return a - b;
+}
+
+
+
+int multiply(int a, int b){
+    return a * b;
+}
+
+
+int divide(int a, int b){
+    return a / b;
+
+}
+
+int main() {
+    // Your code here
+    int a, b;
+    char op;
+  
+    printf("Enter a character and the operation that you want to do: ");
+    scanf("%d %d %c", &a, &b, &op);
+
+    if(op == '+'){
+        printf("%d", add(a, b));
+    } else if (op == '-'){
+        printf("%d", substract(a, b));
+    } else if (op == '*'){
+        printf("%d", multiply(a, b));
+
+    } else if (op == '/' && b == 0){
+        printf("Invalid input");
+    }
+     else if (op == '/'){
+        printf("%d", divide(a, b));
+    } else {
+        printf("Invalid input");
+    }
+
+    return 0;
+}
+
