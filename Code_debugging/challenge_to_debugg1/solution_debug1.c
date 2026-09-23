425783707c4e8e276dd0a7b62dcbc64c3aeaa191
Ddoggen6 <berninimarianosanchezperez@gmail.com>
2026-09-22 21:49:58 -0400

    I think that this is a better practice for today coding, better than writing 1,000 lines of code without an end

---
+++ Code_debugging/challenge_to_debugg1/solution_debug1.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_STUDENTS 5
+#define MAX_NAME 50
+
+void inputStudents(char names[][MAX_NAME], int grades[], int size);
+void printStudents(char names[][MAX_NAME], int grades[], int size);
+float calculateAverage(int grades[], int size);
+int findHighest(int grades[], int size);
+int findLowest(int grades[], int size);
+int findStudent(char names[][MAX_NAME], int size, char *searchName);
+void toUpperCase(char *name);
+
+int main() {
+    char names[MAX_STUDENTS][MAX_NAME];
+    int grades[MAX_STUDENTS];
+
+    int highest;
+    int lowest;
+    float average;
+
+    printf("=== STUDENT GRADE MANAGER ===\n\n");
+
+    inputStudents(names, grades, MAX_STUDENTS);
+
+    printf("\n=== STUDENTS ===\n");
+    printStudents(names, grades, MAX_STUDENTS);
+
+    average = calculateAverage(grades, MAX_STUDENTS);
+    highest = findHighest(grades, MAX_STUDENTS);
+    lowest = findLowest(grades, MAX_STUDENTS);
+
+    printf("\nAverage grade: %.2f\n", average);
+    printf("Highest grade: %d\n", highest);
+    printf("Lowest grade: %d\n", lowest);
+
+    char searchName[MAX_NAME];
+
+    printf("\nEnter a student name to search: ");
+    scanf("%49s", searchName);
+
+    int position = findStudent(names, MAX_STUDENTS, searchName);
+
+    if (position != -1) {
+        printf("\nStudent found!\n");
+        printf("Name: %s\n", names[position]);
+        printf("Grade: %d\n", grades[position]);
+    } else {
+        printf("\nStudent not found.\n");
+    }
+
+    return 0;
+}
+
+void inputStudents(char names[][MAX_NAME], int grades[], int size) {
+
+    for (int i = 0; i < size; i++) {
+        printf("Enter name of student %d: ", i + 1);
+        scanf("%49s", names[i]);
+
+        printf("Enter grade for %s: ", names[i]);
+        scanf("%d", &grades[i]);
+
+        if (grades[i] < 0 || grades[i] > 100) {
+            printf("Invalid grade!\n");
+            grades[i] = 0;
+        }
+    }
+}
+
+void printStudents(char names[][MAX_NAME], int grades[], int size) {
+
+    for (int i = 0; i < size; i++) {
+        printf("%d. %s - %d\n", i + 1, names[i], grades[i]);
+    }
+}
+
+float calculateAverage(int grades[], int size) {
+
+    int total = 0;
+
+    for (int i = 0; i < size; i++) {
+        total += grades[i];
+    }
+
+    return (float) total / size;
+}
+
+int findHighest(int grades[], int size) {
+
+    int highest = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (grades[i] > highest) {
+            highest = grades[i];
+        }
+    }
+
+    return highest;
+}
+
+int findLowest(int grades[], int size) {
+
+    int lowest = 100;
+
+    for (int i = 0; i < size; i++) {
+        if (grades[i] < lowest) {
+            lowest = grades[i];
+        }
+    }
+
+    return lowest;
+}
+
+int findStudent(char names[][MAX_NAME], int size, char *searchName) {
+
+    for (int i = 0; i < size; i++) {
+
+        if (strcmp(names[i], searchName) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void toUpperCase(char *name) {
+
+    for (int i = 0; i < strlen(name); i++) {
+        name[i] = toupper(name[i]);
+    }
+}
